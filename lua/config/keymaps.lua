912ea6338a16a50e5fa281b9336940c2104e3323
ocrossi <zhdoctor971@gmail.com>
2024-10-19 20:14:32 +0200

    better colors + tmux plugin & keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,15 @@ local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
 
+-- local M = {}
+--
+-- M.general = {
+--   n = {
+--     ["<C-h>"] = { "<cmd> TmuxNavigationLeft<CR>", "window left" },
+--     ["<C-l>"] = { "<cmd> TmuxNavigationRight<CR>", "window right" },
+--   }
+-- }
+
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
@@ -16,3 +25,13 @@ keymap("n", "<F4>", ":tabnew ", opts)
 keymap("i", "<F2>", "<esc>:tabprev<CR>", opts)
 keymap("i", "<F3>", "<esc>:tabnext<CR>", opts)
 keymap("i", "<F4>", "<esc>:tabnew ", opts)
+
+keymap("n", "<C-e>", "$", opts)
+keymap("i", "<C-e>", "<esc>$i", opts)
+keymap("n", "<C-a>", "0", opts)
+keymap("i", "<C-a>", "<esc>0i", opts)
+
+-- keymap("n", "<C-h>", "<cmd> TmuxNavigationLeft<CR>", opts)
+-- keymap("n", "<C-j>", "<cmd> TmuxNavigationRight<CR>", opts)
+-- keymap("n", "<C-k>", "<cmd> TmuxNavigationDown<CR>", opts)
+-- keymap("n", "<C-l>", "<cmd> TmuxNavigationUp<CR>", opts)
